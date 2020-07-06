33e39790443a889fddba21552fb356af5e5901f2
plkujaw <jawor.jakub@gmail.com>
2020-07-06 15:44:30 +0100

    setup deadline date

--- app.js
+++ app.js
@@ -21,3 +21,29 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const revolution = document.querySelector(".revolution");
+const deadline = document.querySelector(".deadline");
+const counter = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2030, 0, 1, 00, 00); //yyyy, m, d, hh, mm, ss, ms
+
+const year = futureDate.getFullYear();
+const month = months[futureDate.getMonth()];
+const weekday = weekdays[futureDate.getDay()];
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+day = (futureDay) => {
+  if (futureDay % 10 === 1 && futureDay !== 11) {
+    return `${futureDay}st`;
+  } else if (futureDay % 10 === 2 && futureDay !== 12) {
+    return `${futureDay}nd`;
+  } else if (futureDay % 10 === 3 && futureDay !== 13) {
+    return `${futureDay}rd`;
+  } else {
+    return `${futureDay}th`;
+  }
+}
+
+revolution.textContent = `revolution starts on ${weekday}, ${day(futureDate.getDate())} ${month} ${year} @0${hours}:0${minutes}`
